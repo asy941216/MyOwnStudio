3b75502afff094a674c3454666fa045316ac9a56
seongyeon <42165220+asy941216@users.noreply.github.com>
2018-08-12 20:57:15 +0900

    20180812

--- GetUsualNumber/GetUsualNumber/main.cpp
+++ GetUsualNumber/GetUsualNumber/main.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 #define MAX 100
-#define NUM 8
+#define NUM 10
+
+struct Array {
+	int AllNum[MAX] = { 0, }; //총 갯수가 MAX개만큼 입력된 랜덤한 숫자
+	int CountNum[NUM] = { 0, }; //배열의 번호 => (번호+1)의 갯수
+	int* SaveAddr[NUM]; //Count의 주소를 저장하는 공간
+	int* MaxAddr = 0; //가장 큰 갯수의 주소를 SaveAddr에서 받아 저장하는 공간
+	int* SameAddr = 0; //
+};
 
 int main()
 {
@@ -62,6 +70,9 @@ int main()
 			SameAddress = AddrStorge[m];
 		}
 	}
+
+	if (Count[NUM - 2] < Count[NUM - 1])
+		MaxAddress = AddrStorge[NUM - 1];
 
 	//결과 출력
 	if (SameAddress > MaxAddress)
