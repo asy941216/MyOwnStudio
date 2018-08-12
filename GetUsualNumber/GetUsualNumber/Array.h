e22bf2136fbb00e4d35e33a4c2d96e96e7962c81
seongyeon <42165220+asy941216@users.noreply.github.com>
2018-08-12 23:02:30 +0900

    ...

--- GetUsualNumber/GetUsualNumber/Array.h
+++ GetUsualNumber/GetUsualNumber/Array.h
@@ -7,5 +7,5 @@ struct Array {
 	int CountNum[NUM] = { 0, }; //배열의 번호 => (번호+1)의 갯수
 	int* SaveAddr[NUM]; //Count의 주소를 저장하는 공간
 	int* MaxAddr = 0; //가장 큰 갯수의 주소를 SaveAddr에서 받아 저장하는 공간
-	int* SameAddr = 0; //가장 큰 수가 2개인 지점을 표시.
+	int* SameAddr = 0; //가장 큰 수가 2개인 지점을 표시.....
 };
